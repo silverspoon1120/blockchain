3a586184de56d17f3c2a8842f77fefb882a0dd64
Silver Spoon <silverspoon1120.sw@gmail.com>
2021-08-16 11:06:16 +0800

    修复错别字：类型—>类似

--- _code/unpack_chaincode.go
+++ _code/unpack_chaincode.go
@@ -1,44 +1,33 @@
-// Usage: ./prog inputPkg outputPkg
-
 package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"github.com/golang/protobuf/proto"
 	pb "github.com/hyperledger/fabric/protos/peer"
-	"io/ioutil"
-	"os"
+
+
 )
 
-func check(msg string, e error) {
+func check(e error) {
 	if e != nil {
-		fmt.Println(msg)
 		panic(e)
 	}
 }
 
 func main() {
-	if len(os.Args) != 3 {
-		fmt.Printf("Usage: %s inputPkg outputPkg\n", os.Args[0])
-		return
-	}
-
-	inputPkg := os.Args[1]
-	outputPkg := os.Args[2]
-	fmt.Printf("Input pkg=%s, output=%s\n", inputPkg, outputPkg)
-
-	ccbytes, err := ioutil.ReadFile(inputPkg)
-	check("Read file failed", err)
-
+    chaincodePkg := "./test_cc.V1"
+    chaincodeTar := "./test_cc.tar"
+	ccbytes, err := ioutil.ReadFile(chaincodePkg)
+	check(err)
 	buf := ccbytes
+
 	depSpec := &pb.ChaincodeDeploymentSpec{}
 	err = proto.Unmarshal(buf, depSpec)
-	check("Unmarshal depSpec failed", err)
-
+	check(err)
 	fmt.Printf("chaincodeSpec=%+v\n", depSpec.ChaincodeSpec)
 
-	payload := depSpec.CodePackage
-	err = ioutil.WriteFile(outputPkg, payload, 0644)
-	check("Write to file failed", err)
+    payload := depSpec.CodePackage
+    err = ioutil.WriteFile(chaincodeTar, payload, 0644)
+    check(err)
 }
-
